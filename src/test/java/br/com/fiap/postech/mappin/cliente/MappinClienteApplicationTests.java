59964af0af660a2cad66c0f38c9f69d5f2e134fc
Anderson0Wagner <anderson.wagner@gmail.com>
2024-05-06 17:25:37 -0300

    removendo teste vazio

--- src/test/java/br/com/fiap/postech/mappin/cliente/MappinClienteApplicationTests.java
+++ src/test/java/br/com/fiap/postech/mappin/cliente/MappinClienteApplicationTests.java
@@ -1,14 +1,9 @@
 package br.com.fiap.postech.mappin.cliente;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Generated
 class MappinClienteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
